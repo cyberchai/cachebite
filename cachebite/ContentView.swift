4fe12f39c83d12b782fe50258bdfc6030914421a
Chaira Kai Harder <68481159+cyberchai@users.noreply.github.com>
2024-04-23 00:31:25 -0400

    Add files via upload

---
+++ cachebite/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  cachebite
+//
+//  Created by Chaira Harder on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cachebite/cachebiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  cachebiteApp.swift
+//  cachebite
+//
+//  Created by Chaira Harder on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct cachebiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
