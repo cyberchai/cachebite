a3089ae3256c89994a966370cd593191d8137d5e
ZihanLyu <llyu69@smith.edu>
2024-04-23 16:10:21 -0400

    login

---
+++ cachebite/HomePage.swift
@@ -0,0 +1,23 @@
+//
+//  HomePage.swift
+//  cachebite
+//
+//  Created by Zihan Lyu on 4/23/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text("Welcome to the Home Page!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
+
